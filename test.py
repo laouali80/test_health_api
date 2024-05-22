88e5cd65609794393262837e1354483644284499
Laouali Bachir <laoualibachir2000@gmail.com>
2024-05-22 11:06:35 +0100

    import all the APIs

--- test.py
+++ test.py
@@ -42,33 +42,57 @@ from coreapi import Client
 from dotenv import load_dotenv
 import os
 
-def configure():
-    load_dotenv()
 
-def main():
-    configure()
- 
+load_dotenv()
 
-main()
+
 
 client = Client()
 schema = client.get("https://healthsites.io/api/docs/")
 
-print(type(os.getenv("api_key")))
-# Interact with the API endpoint
+# Get all the health facilities
 action = ["facilities", "list"]
 params = {
-    "api-key": os.getenv("api_key"),
-    "page": 1,
-    # "country": ...,
-    # "extent": ...,
-    # "from": ...,
-    # "to": ...,
-    # "flat-properties": ...,
-    # "tag-format": ...,
-    # "output": ...,
+    "api-key": os.getenv("api_key"),           # Api_key
+    "page": 1,                                 # number of page
+    # "country": "Nigeria",                      # Replace with the specific country name or code
+    # "extent": "minLng,minLat,maxLng,maxLat", # Replace with the geographic extent (e.g., "-123.3656,48.4284,-122.3656,49.4284")
+    # "from": "2024-01-01",                    # Replace with the start date in YYYY-MM-DD format
+    # "to": "2024-12-31",                      # Replace with the end date in YYYY-MM-DD format
+    # "flat-properties": True,                 # Boolean value to flatten properties (True/False)
+    # "tag-format": "osm",                     # Tag format (e.g., "osm")
+    # "output": "json"                         # Output format (e.g., "json")
 }
-result = client.action(schema, action, params=params)
-# result = client.action(schema)
 
-# print(result)
+
+# Get a specific health facility
+# action = ["facilities", "read"]
+# params = {
+#     "osm_type": ...,
+#     "osm_id": ...,
+#     "api-key": ...,
+# }
+
+
+# Post an update facility (POST request)
+# action = ["facilities", "read"]
+# # those parameters should be in the url
+# params = {
+#     "osm_type": ...,
+#     "osm_id": ...,
+#     "api-key": ...,
+# }
+
+
+# Download a shapefile of a country facilities (state,local.....)
+# action = ["shapefile", "download"]
+# params = {
+#     "country": ...,
+#     "api-key": ...,
+# }
+
+
+
+result = client.action(schema, action, params=params)
+
+print(result)
